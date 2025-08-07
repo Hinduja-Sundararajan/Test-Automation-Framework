7509c55cc0cb8b896b777874ec907f05ee37ebd9
Hinduja <Hinduja@DESKTOP-MR0HIJM>
2025-08-07 11:12:39 +0530

    Merge branch 'master' of https://github.com/Hinduja-Sundararajan/Test-Automation-Framework.git

--- src/test/java/com/utilities/BrowserUtilities.java
+++ src/test/java/com/utilities/BrowserUtilities.java
@@ -103,7 +103,7 @@ public abstract class BrowserUtilities {
 		Date date = new Date();
 		SimpleDateFormat format = new SimpleDateFormat("HH-mm-ss");
 		String timeStamp = format.format(date);
-		String path = System.getProperty("user.dir") + "//screenshots//" + name + "-" + timeStamp + ".png";
+		String path =  "./screenshots/" + name + "-" + timeStamp + ".png";
 		File screenshotdst = new File(path);
 		try {
 			FileUtils.copyFile(screenshotsrc, screenshotdst);
